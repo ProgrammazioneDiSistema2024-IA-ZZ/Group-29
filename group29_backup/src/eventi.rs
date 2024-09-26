3c9208f1b62b2399c51f5cd76235dfaa737df1e7
VaglioFabiano <100426932+VaglioFabiano@users.noreply.github.com>
2024-09-26 15:44:16 +0200

    Update eventi.rs

--- group29_backup/src/eventi.rs
+++ group29_backup/src/eventi.rs
@@ -1,4 +1,7 @@
 use rdev::{listen, Event, EventType};
+use winit::event_loop::{EventLoop, ControlFlow};
+use winit::monitor::MonitorHandle;
+use winit::window::WindowBuilder;
 
 // Funzione per gestire gli eventi del mouse
 pub fn handle_mouse_events() {
@@ -21,6 +24,36 @@ fn callback(event: Event) {
         }
         _ => (),
     }
+}
+
+// Funzione per ottenere la dimensione dello schermo e monitor principale
+pub fn get_screen_dimensions() {
+    // Crea un nuovo EventLoop
+    let event_loop = EventLoop::new();
+
+    // Crea una finestra invisibile
+    let window = WindowBuilder::new()
+        .with_visible(false) // La finestra è nascosta perché non ci serve
+        .build(&event_loop)
+        .expect("Impossibile creare la finestra");
+
+    // Ottiene il monitor principale
+    let monitor = window.primary_monitor();
+
+    // Recupera la risoluzione dello schermo
+    let (screen_width, screen_height) = match monitor {
+        Some(monitor) => {
+            let size = monitor.size();
+            (size.width as f64, size.height as f64)
+        }
+        None => {
+            println!("Impossibile ottenere la risoluzione dello schermo!");
+            return;
+        }
+    };
+
+    // Stampa le dimensioni dello schermo
+    println!("Risoluzione dello schermo: {}x{}", screen_width, screen_height);
 }
 
 /* Versione senza premere il mouse
