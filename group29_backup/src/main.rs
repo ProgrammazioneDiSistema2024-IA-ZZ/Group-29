2e8913f2470cccabd2b234052fd2b7f6f41805de
francescasaglimbeni <s329977@studenti.polito.it>
2024-10-17 12:40:42 +0200

    directory 2

--- group29_backup/src/main.rs
+++ group29_backup/src/main.rs
@@ -6,16 +6,24 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Ottieni il percorso della directory di lavoro corrente
+    let current_dir = env::current_dir()?;
 
-    let mut current_dir = env::current_dir().unwrap().parent().unwrap().to_path_buf();
-    current_dir.push("config.toml");
+    // Ottieni la directory superiore (rimuovi la parte 'target')
+    let project_dir = current_dir.parent()
+        .ok_or("Impossibile ottenere la directory superiore")?
+        .parent()
+        .ok_or("Impossibile ottenere la directory del progetto")?;
 
-    let config = backup::BackupConfig::load_from_file(current_dir.to_str().ok_or("Percorso non valido")?)?;
+    // Costruisci il percorso del file di configurazione
+    let config_path = project_dir.join("config.toml");
+    let config = backup::BackupConfig::load_from_file(config_path.to_str().ok_or("Percorso non valido")?)?;
 
-    // Imposta la destinazione del backup
-    let destination = "C:/Users/Fabiano Vaglio/Desktop/destinazione";
+    // Specifica il percorso della chiavetta USB o di una cartella specifica
+    let usb_destination = PathBuf::from("C:/Users/sagli/Desktop/uni/PROGRAMMAZIONE DI SISTEMA/SECONDA PARTE/Progetto/destinazione"); // Cambia "D:/" con la lettera della tua chiavetta USB
+
     // Esegui il backup
-    backup::perform_backup(&config, destination)?;
+    backup::perform_backup(&config, usb_destination.to_str().ok_or("Percorso non valido")?)?;
 
     Ok(())
 }
