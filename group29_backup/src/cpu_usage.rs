e4497efb0282e084d170c663ccf946752fcb2e61
FRANCESCO PASSIATORE s332821 <s332821@studenti.polito.it>
2024-11-27 11:05:34 +0100

    Correzione cpu_usage

--- group29_backup/src/cpu_usage.rs
+++ group29_backup/src/cpu_usage.rs
@@ -6,31 +6,15 @@ use std::thread;
 
 pub fn log_cpu_usage() {
     let mut system = System::new_all();
-    /*
+
+    // Ottieni il PID del processo corrente
     let pid = Pid::from(std::process::id() as usize);
-    //COMMIT
+
     loop {
-        system.refresh_process(pid);
+        // Aggiorna le informazioni del sistema
+        system.refresh_all();
 
-        // Ottieni il processo corrente usando il PID
-        if let Some(process) = system.process(pid) {
-            let cpu_usage = process.cpu_usage(); // Uso della CPU del processo corrente
-
-            // Apri o crea il file di log
-            let mut file = OpenOptions::new()
-                .append(true)
-                .create(true)
-                .open("process_cpu_usage.log")
-                .expect("Impossibile aprire il file di log");
-
-            // Scrivi l'uso della CPU nel file di log
-            writeln!(file, "Consumo di CPU del processo: {:.2}%", cpu_usage)
-                .expect("Impossibile scrivere nel file di log");
-        } else {
-            eprintln!("Impossibile trovare il processo con PID: {}", pid);
-        }
-
-        */
+        // Log dell'uso della CPU totale
         let cpu_usage = system.cpus().iter().map(|cpu| cpu.cpu_usage()).sum::<f32>() / system.cpus().len() as f32;
 
         let mut file = OpenOptions::new()
@@ -39,12 +23,22 @@ pub fn log_cpu_usage() {
             .open("cpu_usage.log")
             .expect("Impossibile aprire il file di log");
 
-        writeln!(file, "Consumo di CPU: {:.2}%", cpu_usage)
+        writeln!(file, "Consumo medio di CPU: {:.2}%", cpu_usage)
             .expect("Impossibile scrivere nel file di log");
 
+        // Log dell'uso della CPU del processo corrente
+        if let Some(process) = system.process(pid) {
+            let process_cpu_usage = process.cpu_usage();
 
+            writeln!(file, "Consumo di CPU del processo: {:.2}%", process_cpu_usage)
+                .expect("Impossibile scrivere nel file di log");
+        } else {
+            writeln!(file, "Impossibile trovare il processo con PID: {}", pid)
+                .expect("Impossibile scrivere nel file di log");
+        }
 
-        // Attendi 10 secondi prima di ripetere il log
+        // Attendi 120 secondi prima di ripetere il log
         thread::sleep(Duration::from_secs(120));
+    }
     }
 
