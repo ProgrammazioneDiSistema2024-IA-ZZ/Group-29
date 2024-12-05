015f5b24dccb462912494c2fdec37ef4fe3bd8ff
francescasaglimbeni <s329977@studenti.polito.it>
2024-12-05 10:52:50 +0100

    project dir

--- group29_backup/src/dir_functions.rs
+++ group29_backup/src/dir_functions.rs
@@ -2,12 +2,22 @@ use std::env;
 use std::path::PathBuf;
 
 pub fn get_project_directory() -> Result<PathBuf, Box<dyn std::error::Error>> {
-    let current_dir = env::current_dir()?;
-    /*let project_dir = current_dir
+    /*let current_dir = env::current_dir()?;
+    let project_dir = current_dir
         .ancestors()
         .nth(1)
         .ok_or("Impossibile ottenere la directory del progetto")?;*/
-    Ok(current_dir.to_path_buf())
+    let exe_path = env::current_exe()?;
+    // Risale alla directory del progetto partendo dall'eseguibile
+    let project_dir = exe_path
+        .parent() // Ottieni la directory dell'eseguibile
+        .ok_or("Impossibile ottenere la directory del progetto.")?
+        .to_path_buf();
+
+    println!("Directory del progetto calcolata: {:?}", project_dir);
+
+    Ok(project_dir)
+    //Ok(current_dir.to_path_buf())
 }
 
 pub fn get_project_directory_sound() -> Result<PathBuf, Box<dyn std::error::Error>> {
--- group29_backup/target/debug/deps/group29_backup.d
+++ group29_backup/target/debug/deps/group29_backup.d
@@ -1,8 +1,8 @@
-C:\Users\Fabiano Vaglio\RustroverProjects\Group-29\group29_backup\target\debug\deps\group29_backup.exe: src\main.rs src\backup.rs src\gui_backup.rs src\mouse_controller.rs src\eventi.rs src\suoni.rs src\cpu_usage.rs src\dir_functions.rs
+C:\Users\sagli\Desktop\uni\PROGRAMMAZIONE DI SISTEMA\SECONDA PARTE\Progetto\Group-29\group29_backup\target\debug\deps\group29_backup.exe: src/main.rs src\backup.rs src\gui_backup.rs src\mouse_controller.rs src\eventi.rs src\suoni.rs src\cpu_usage.rs src\dir_functions.rs
 
-C:\Users\Fabiano Vaglio\RustroverProjects\Group-29\group29_backup\target\debug\deps\group29_backup.d: src\main.rs src\backup.rs src\gui_backup.rs src\mouse_controller.rs src\eventi.rs src\suoni.rs src\cpu_usage.rs src\dir_functions.rs
+C:\Users\sagli\Desktop\uni\PROGRAMMAZIONE DI SISTEMA\SECONDA PARTE\Progetto\Group-29\group29_backup\target\debug\deps\group29_backup.d: src/main.rs src\backup.rs src\gui_backup.rs src\mouse_controller.rs src\eventi.rs src\suoni.rs src\cpu_usage.rs src\dir_functions.rs
 
-src\main.rs:
+src/main.rs:
 src\backup.rs:
 src\gui_backup.rs:
 src\mouse_controller.rs:
